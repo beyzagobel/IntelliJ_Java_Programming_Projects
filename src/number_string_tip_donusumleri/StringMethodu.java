9e34f894448f80a568cc98d6c8c544a9bf1560a2
byzgbl <byzgbll@gmail.com>
2021-10-24 13:37:43 +0300

    update

--- src/number_string_tip_donusumleri/StringMethodu.java
+++ src/number_string_tip_donusumleri/StringMethodu.java
@@ -2,6 +2,8 @@ package number_string_tip_donusumleri;
 
 public class StringMethodu {
     public static void main(String[] args) {
+
+        // toString(metodu uygulayarak farklı veri tiplerini metinsel ifadelere dönüştürmüş olduk)
         int i=1;
         double d=0.2;
         float f=0.3f;
@@ -9,13 +11,21 @@ public class StringMethodu {
         byte b=5;
 
         Character c='g';
-        String metinChar=c.toString();  // bu şekildede tip dönüşümü yapılabilir.  // nesne üzerinden üye method
+        String metinChar=c.toString();  // bu şekildede nesne üzerinden erişilerek tip dönüşümü yapılabilir.  //  üye method
+        System.out.println(metinChar);
 
-        String metinInt=Integer.toString(i);   // sınıf üzerinden erişim
+
+        String metinInt=Integer.toString(i);   // sınıf üzerinden erişim  // static metod
         String metindouble=Double.toString(d);
         String metinfloat=Float.toString(f);
         String metinshort=Short.toString(s);
         String metinbyte=Byte.toString(b);
+
+        System.out.println(metinInt);
+        System.out.println(metindouble);
+        System.out.println(metinfloat);
+        System.out.println(metinshort);
+        System.out.println(metinbyte);
 
     }
 }
