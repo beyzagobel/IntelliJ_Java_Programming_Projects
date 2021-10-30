86740e1a349ead355910e754adec33198cd6dcc9
byzgbl <byzgbll@gmail.com>
2021-10-30 23:27:36 +0300

    Merge remote-tracking branch 'origin/master'

--- src/donguler/WhileDongusu.java
+++ src/donguler/WhileDongusu.java
@@ -1,5 +1,8 @@
 package donguler;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class WhileDongusu {
     public static void main(String[] args) {
         /*
@@ -11,6 +14,10 @@ public class WhileDongusu {
         int i=1;
         while(i<=100){
             System.out.println();
+
+            Scanner scanner=new Scanner(System.in);
+            Locale locale=new Locale("tr","TR");
+
         }
     }
 }
